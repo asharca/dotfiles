ccdb38db2ec91ccd24198f59270d6fa91b823984
asharca <129170100+asharca@users.noreply.github.com>
2025-11-17 16:22:10 +0800

    disable autoformat for .env*

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { ".*" },
+  callback = function(ev)
+    -- 获取文件名（不含路径）
+    local name = vim.fn.fnamemodify(ev.file, ":t")
+
+    -- 如果以 "." 开头，例如 .env, .env.local, .gitignore 等
+    if name:sub(1, 1) == "." then
+      vim.b[ev.buf].autoformat = false
+    end
+  end,
+})
