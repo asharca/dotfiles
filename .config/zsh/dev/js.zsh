02c7697ae5f798507edc58c98a5d71f02b6d0166
asharca <129170100+asharca@users.noreply.github.com>
2025-12-10 10:18:58 +0800

    update

--- .config/zsh/dev/js.zsh
+++ .config/zsh/dev/js.zsh
@@ -9,3 +9,7 @@ fi
 # Yarn
 [[ -d "$HOME/.yarn/bin" ]] && export PATH="$HOME/.yarn/bin:$PATH"
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
